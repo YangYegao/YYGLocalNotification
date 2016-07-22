0b84b6300a57320bb35857a58dee0ef6c98dac52
Jack <jack@JackdeMac-mini.local>
2016-07-22 13:49:43 +0800

    first commit

--- YYGLocalNotification/AppDelegate.m
+++ YYGLocalNotification/AppDelegate.m
@@ -16,13 +16,35 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    //after iOS8, we need do that
+    UIUserNotificationType type = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:type categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    
     return YES;
 }
 
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
+    if (notification.alertBody) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Tips" message:@"It's time to study" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"YES" style:UIAlertActionStyleCancel handler:nil];
+        [alert addAction:action];
+        
+        UIViewController *topPresentedViewController = self.window.rootViewController;
+        while (topPresentedViewController.presentedViewController) {
+            topPresentedViewController = topPresentedViewController.presentedViewController;
+        }
+        [topPresentedViewController presentViewController:alert animated:YES completion:nil];
+    }
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    application.applicationIconBadgeNumber = 0;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    application.applicationIconBadgeNumber = 0;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -32,10 +54,6 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
--- YYGLocalNotification/ViewController.m
+++ YYGLocalNotification/ViewController.m
@@ -8,16 +8,51 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *button;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.textField.delegate = self;
+
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeButtonState) name:UITextFieldTextDidChangeNotification object:nil];
 }
+
+- (IBAction)setupLocalNotifiction:(id)sender {
+    
+    NSTimeInterval interval = [self.textField.text doubleValue] * 60;
+    UILocalNotification *local = [[UILocalNotification alloc] init];
+    local.fireDate = [NSDate dateWithTimeIntervalSinceNow:interval];
+    local.alertBody = @"It's time to study";
+    local.alertAction = @"关闭";
+    local.alertTitle = @"Tips";
+    local.soundName = UILocalNotificationDefaultSoundName;
+    local.applicationIconBadgeNumber = 1;
+    [[UIApplication sharedApplication] scheduleLocalNotification:local];
+    
+    self.textField.text = nil;
+    self.button.enabled = NO;
+    [self.textField resignFirstResponder];
+}
+
+
+- (void)changeButtonState{
+    if (self.textField.text.length > 0) {
+        self.button.enabled = YES;
+    }
+    else{
+        self.button.enabled = NO;
+    }
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.textField resignFirstResponder];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
