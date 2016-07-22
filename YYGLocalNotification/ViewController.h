595a977a03cfe5529fa7dcc599d30f973fcb891f
Jack <jack@jackdemac-mini.local>
2016-07-22 13:37:04 +0800

    Initial Commit

---
+++ YYGLocalNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYGLocalNotification
+//
+//  Created by Jack on 16/7/22.
+//  Copyright © 2016年 HaiDaFu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YYGLocalNotification/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YYGLocalNotification
+//
+//  Created by Jack on 16/7/22.
+//  Copyright © 2016年 HaiDaFu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
